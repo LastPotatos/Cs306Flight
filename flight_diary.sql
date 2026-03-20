37947d2bbd500da0d0a016fe1837badb63459f5b
LastPotatos <hanaga571@gmail.com>
2026-03-20 15:10:18 +0300

    added 2 more tables

--- flight_diary.sql
+++ flight_diary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2026 at 12:32 PM
+-- Generation Time: Mar 20, 2026 at 01:09 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
