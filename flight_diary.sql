aa19c72edea401c0153b132bc75d78e17cd12666
LastPotatos <hanaga571@gmail.com>
2026-03-21 11:20:17 +0300

    finished phase 2 build

--- flight_diary.sql
+++ flight_diary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2026 at 01:09 PM
+-- Generation Time: Mar 21, 2026 at 09:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `flight diary`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aircraft`
+--
+
+CREATE TABLE `aircraft` (
+  `Registration-Number` varchar(8) NOT NULL,
+  `ICAO code of the airline` varchar(3) NOT NULL,
+  `Type` varchar(15) NOT NULL,
+  `SeetConfigs` text NOT NULL,
+  `Capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airline`
+--
+
+CREATE TABLE `airline` (
+  `Name` text NOT NULL,
+  `ICAO` varchar(3) NOT NULL,
+  `Fleet Size` int(11) NOT NULL,
+  `IATA code` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,6 +71,9 @@ CREATE TABLE `airport` (
 
 CREATE TABLE `flight` (
   `E-mail` varchar(100) NOT NULL,
+  `Aircraft-Registration` varchar(8) NOT NULL,
+  `D_Airport_ICAO` varchar(6) NOT NULL,
+  `A_Airport_ICAO` varchar(6) NOT NULL,
   `Date` datetime NOT NULL,
   `Flight Number` varchar(6) NOT NULL,
   `SDeparture` datetime NOT NULL,
@@ -103,12 +133,28 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `aircraft`
+--
+ALTER TABLE `aircraft`
+  ADD PRIMARY KEY (`Registration-Number`),
+  ADD UNIQUE KEY `ICAO code of the airline` (`ICAO code of the airline`);
+
+--
+-- Indexes for table `airline`
+--
+ALTER TABLE `airline`
+  ADD PRIMARY KEY (`ICAO`);
+
+--
 -- Indexes for table `flight`
 --
 ALTER TABLE `flight`
   ADD PRIMARY KEY (`Flight Number`,`Date`),
   ADD UNIQUE KEY `Date` (`Date`,`Flight Number`),
-  ADD UNIQUE KEY `E-mail` (`E-mail`);
+  ADD UNIQUE KEY `E-mail` (`E-mail`),
+  ADD UNIQUE KEY `Aircraft-Registration` (`Aircraft-Registration`),
+  ADD UNIQUE KEY `A_Airport_ICAO` (`A_Airport_ICAO`),
+  ADD UNIQUE KEY `D_Airport_ICAO` (`D_Airport_ICAO`);
 
 --
 -- Indexes for table `path`
@@ -136,10 +182,17 @@ ALTER TABLE `user`
 --
 
 --
+-- Constraints for table `aircraft`
+--
+ALTER TABLE `aircraft`
+  ADD CONSTRAINT `owns relation` FOREIGN KEY (`ICAO code of the airline`) REFERENCES `airline` (`ICAO`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `flight`
 --
 ALTER TABLE `flight`
-  ADD CONSTRAINT `Logs Relation` FOREIGN KEY (`E-mail`) REFERENCES `user` (`E-mail`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Logs Relation` FOREIGN KEY (`E-mail`) REFERENCES `user` (`E-mail`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `uses relation` FOREIGN KEY (`Aircraft-Registration`) REFERENCES `aircraft` (`Registration-Number`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `path`
